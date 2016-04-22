dff9c8692febf9193c0907733737d59f035196c4
Edvon <edvin.ljungstrand@hotmail.com>
2016-04-22 16:29:52 +0200

    lagt till ny fil med asteroid-klass

---
+++ asteroids.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require racket/gui)
+(provide asteroid%)
+
+(define asteroid%
+  (class object%
+    [init-field [image (make-bitmap 100 100)]
+                [x 0]
+                [y 0]]))
+
+
+
+
+
+(define (asteroid-image bitmap-target)
+  ((let ((dc (new bitmap-dc% [bitmap bitmap-target])))
+    (send dc set-brush (make-object brush% "orange" 'solid))
+    (send dc draw-ellipse 10 10 60 60))))
+    
+
+                 
+       
