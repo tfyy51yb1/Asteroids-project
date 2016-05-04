a80c66117d9cbead3fd9fba5f4bc3c0941e61bf9
Oscar <noboru_ankare@outlook.com>
2016-05-04 15:50:53 +0200

    Moved update-object-methods from game% to each object.

--- game.rkt
+++ game.rkt
@@ -22,117 +22,21 @@
           [(> ypos 1080) (send obj set-mid-y! 0)]
           [(< ypos 0) (send obj set-mid-y! 1080)])))
     
-    ;; update-ship uses parameters provided by the
-    ;; ship object, calculates the ship's new position
-    ;; and renders it.
-    (define (update-ship ship dc)
-      (let* ([image (send ship get-image)]
-             [image-width (send image get-width)]
-             [image-height (send image get-height)]
-             [xpos (get-field xpos ship)]
-             [ypos (get-field ypos ship)]
-             [dx (get-field dx ship)]
-             [dy (get-field dy ship)]
-             [angle (get-field angle ship)]
-             [speed (get-field speed ship)])
-        
-        ;; Drawing
-        (send dc translate (+ xpos (/ image-width 2))
-              (+ ypos (/ image-height 2)))
-        (send dc rotate angle)
-        (send dc draw-bitmap image (- (/ image-width 2))
-              (- (/ image-height 2)))
-        (send dc rotate (- angle))
-        (send dc translate (- (+ xpos (/ image-width 2)))
-              (- (+ ypos (/ image-height 2))))
-        
-        ;; Physics
-        (set! xpos (+ xpos dx))
-        (set! ypos (+ ypos dy))
-        (set! dx (* dx 0.99))
-        (set! dy (* dy 0.99))
-        (set-field! xpos ship xpos)
-        (set-field! ypos ship ypos)
-        (set-field! dx ship dx)
-        (set-field! dy ship dy)
-        (screen-wrap ship)))
     
-    ;; update-ship uses parameters provided by
-    ;; bullet objects, which are stored in the bullet-hash,
-    ;; calculates the bullets' new positions and draws them.
-    ;; Currently, eh, unusable, to say the least...
-    (define (update-bullets bullet-hash dc)
-      (for-each (lambda (bullet)
-                  (let*  ([bullet-name (get-field name bullet)]
-                          [health (get-field health bullet)]
-                          [image (send bullet get-image)]
-                          [image-width (send image get-width)]
-                          [image-height (send image get-height)]
-                          [angle (get-field angle bullet)]
-                          [xpos  (get-field xpos bullet)]
-                          [ypos (get-field ypos bullet)]
-                          [dx (get-field dx bullet)]
-                          [dy (get-field dy bullet)])
-                    
-                    ;; Drawing
-                    (send dc draw-bitmap image xpos ypos)
-                    
-                    ;; Logic
-                    (set! health (- health 0.005))
-                    (set-field! health bullet health)
-                    (when (< health 0.001)
-                      (send bullet destroy-bullet bullet-name))
-                    
-                    ;; Physics
-                    (set! xpos (+ xpos dx))
-                    (set! ypos (+ ypos dy))
-                    (set-field! xpos bullet xpos)
-                    (set-field! ypos bullet ypos)
-                    (screen-wrap bullet)))
-                (hash-values bullet-hash)))
     
-    ;; update-asteroids uses parameters provided by
-    ;; asteroid objects, which are stored in the asteroid-hash,
-    ;; calculates the asteroids' new positions and draws them.
-    (define (update-asteroids asteroids-hash dc)
-      (for-each (lambda (asteroid)
-                  (let* ([asteroid-name (get-field name asteroid)]
-                         [health (get-field health asteroid)]
-                         [image (send asteroid get-image)]
-                         [image-width (send image get-width)]
-                         [image-height (send image get-height)]
-                         [xpos  (get-field xpos asteroid)]
-                         [ypos (get-field ypos asteroid)]
-                         [dx (get-field dx asteroid)]
-                         [dy (get-field dy asteroid)])
-                    
-                    ;; Drawing
-                    (send dc draw-bitmap image xpos ypos)
-
-                     
-                    ;; Logic
-                    (set! health (- health 0.005))
-                    (set-field! health asteroid health)
-                    (when (< health 0.001)
-                      (send asteroid destroy-asteroid asteroid-name))
-                    
-                    
-                    ;; Physics
-                    (set! xpos (+ xpos dx))
-                    (set! ypos (+ ypos dy))
-                    
-                    (set-field! xpos asteroid xpos)
-                    (set-field! ypos asteroid ypos)
-                    (set-field! dy asteroid dy)
-                    (set-field! dx asteroid dx)
-                    (screen-wrap asteroid)))
-                (hash-values asteroids-hash)))
     
     ;; The acctual rendering method. Is called by the on-paint method
     ;; provided by the game-canvas% class. render just calls the update methods
     ;; above.
     (define/public (render ship bullet-hash asteroids-hash dc)
-      (update-ship ship dc)
-      (update-asteroids asteroids-hash dc)
-      (update-bullets bullet-hash dc))
-    (super-new)))
+      (send ship update-ship dc)
+      (screen-wrap ship)
+      (for-each (lambda (asteroid)
+                  (send asteroid update-asteroid dc)
+                  (screen-wrap asteroid))
+                (hash-values asteroids-hash))
+      (for-each (lambda (bullet)
+                  (send bullet update-bullet dc)
+                  (screen-wrap bullet))
+                (hash-values bullet-hash)))
+      (super-new)))
--- ship.rkt
+++ ship.rkt
@@ -35,10 +35,10 @@
         (send dc draw-line
               100 100
               50 75)))
-
+    
     ;; The x and y coordinates for middle of the ship bitmap and for the
     ;;tip of the ship.
-
+    
     (define/public (mid-x)
       (+ xpos 50))
     (define/public (mid-y)
@@ -52,36 +52,60 @@
       (- (send this mid-x) (* 50 (sin angle))))
     (define/public (tip-ypos)
       (- (send this mid-y) (* 50 (cos angle))))
-      
     
-    ;; Currently out of order... The idea is to create instances of the bullet%
-    ;; class when the ship fires, add these to a hash table and then update
-    ;; everything in the hash table by using game%. We need to find som expresion
-    ;; so that bullets are created at the front of the ship, irregardless of the
-    ;; ship's direction.
-    (define/private (fire)
-      (let ([bullet-name (gensym "bullet")])
-        (hash-set! bullet-hash bullet-name
-                   (make-object bullet% 1
-                     (send this tip-xpos) (send this tip-ypos) (- (* 20 (sin angle))) (- (* 20 (cos angle))) bullet-name))))
-
     
-    ;; Method for handling key-events. When the 'w' key is pressed we increase
-    ;; the speed of the ship, unless the ship is going to fast already, and
-    ;; determine the value of the speed in both the x and y-direction.
-    ;; When either'a' or 'd' are pressed we turn the ship.
-    (define/public (move key-code)
-      (case key-code
-        [(#\w) (cond ((> speed 1)
-                      (set! speed 1))
-                     (else
-                      (set! speed (+ speed 0.35))
-                      (set! dy (- dy (* speed (cos angle))))
-                      (set! dx (- dx (* speed (sin angle))))))]
-        [(#\a) (set! angle (+ angle 0.2))]
-        [(#\d) (set! angle (- angle 0.2))]
-        [(#\space) (fire)]))
-    
-    (create-ship-image image)
-    
-    (super-new)))
+    ;; update-ship uses parameters provided by the
+    ;; ship object, calculates the ship's new position
+    ;; and renders it.
+    (define/public (update-ship dc)
+      (let*  ([image-width (send image get-width)]
+              [image-height (send image get-height)])
+        
+        ;; Drawing
+        (send dc translate (+ xpos (/ image-width 2))
+              (+ ypos (/ image-height 2)))
+        (send dc rotate angle)
+        (send dc draw-bitmap image (- (/ image-width 2))
+              (- (/ image-height 2)))
+        (send dc rotate (- angle))
+        (send dc translate (- (+ xpos (/ image-width 2)))
+              (- (+ ypos (/ image-height 2))))
+        
+        ;; Physics
+        (set! xpos (+ xpos dx))
+        (set! ypos (+ ypos dy))
+        (set! dx (* dx 0.99))
+        (set! dy (* dy 0.99))))
+        
+        
+        ;; Currently out of order... The idea is to create instances of the bullet%
+        ;; class when the ship fires, add these to a hash table and then update
+        ;; everything in the hash table by using game%. We need to find som expresion
+        ;; so that bullets are created at the front of the ship, irregardless of the
+        ;; ship's direction.
+        (define/private (fire)
+          (let ([bullet-name (gensym "bullet")])
+            (hash-set! bullet-hash bullet-name
+                       (make-object bullet% 1
+                         (send this tip-xpos) (send this tip-ypos) (- (* 20 (sin angle))) (- (* 20 (cos angle))) bullet-name))))
+        
+        
+        ;; Method for handling key-events. When the 'w' key is pressed we increase
+        ;; the speed of the ship, unless the ship is going to fast already, and
+        ;; determine the value of the speed in both the x and y-direction.
+        ;; When either'a' or 'd' are pressed we turn the ship.
+        (define/public (move key-code)
+          (case key-code
+            [(#\w) (cond ((> speed 1)
+                          (set! speed 1))
+                         (else
+                          (set! speed (+ speed 0.35))
+                          (set! dy (- dy (* speed (cos angle))))
+                          (set! dx (- dx (* speed (sin angle))))))]
+            [(#\a) (set! angle (+ angle 0.2))]
+            [(#\d) (set! angle (- angle 0.2))]
+            [(#\space) (fire)]))
+        
+        (create-ship-image image)
+        
+        (super-new)))
